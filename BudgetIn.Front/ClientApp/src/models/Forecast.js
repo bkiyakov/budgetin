6890e0bdf1c135c5cd65dd4d0be8b018e64f9b03
bkiyakov <bkiyakov@yandex.ru>
2020-08-20 13:59:21 +0300

    added vue + net core 3.1 template for Frontoffice

---
+++ BudgetIn.Front/ClientApp/src/models/Forecast.js
@@ -0,0 +1,9 @@
+export class Forecast {
+    constructor(date, temperatureC, temperatureF, summary) {
+        this.date = date;
+        this.temperatureC = temperatureC;
+        this.temperatureF = temperatureF;
+        this.summary = summary;
+    }
+}
+//# sourceMappingURL=Forecast.js.map
